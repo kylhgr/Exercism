5a4d2d90ee00988d3dd4fcd74afcd43a86fff545
Kaylah Gore <5260333-kaylahgore@users.noreply.gitlab.com>
2022-04-12 10:45:01 -0500

    first commit

---
+++ rust/lucians-luscious-lasagna/target/rls/debug/deps/lucians_luscious_lasagna-b04b5d513e4a909b.d
@@ -0,0 +1,5 @@
+/Users/kag73/Exercism/rust/lucians-luscious-lasagna/target/rls/debug/deps/lucians_luscious_lasagna-b04b5d513e4a909b.rmeta: tests/lucians-luscious-lasagna.rs
+
+/Users/kag73/Exercism/rust/lucians-luscious-lasagna/target/rls/debug/deps/lucians_luscious_lasagna-b04b5d513e4a909b.d: tests/lucians-luscious-lasagna.rs
+
+tests/lucians-luscious-lasagna.rs:
